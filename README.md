f64b92cd59dfc6cf0291919255a4668a04ff9e50
AdiM <adi5@gmx.at>
2018-07-09 14:50:09 +0200

    automated pdf-generation

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This is what the questionscrambler does: It takes questions out of a questionsfile, scrambles their order and puts them in a Latex compilable .tex-file.
-Python and LAtex need to be installed on your system in order to perdorm the code generation and compilation afterwards.
-Please place a picture with the name europe.jpg in your home-directory and adapt Line 19 in ./templates/document_begin.tex in order to have a successful compilation.
+This is what the questionscrambler does: It takes questions out of a questionsfile, scrambles their order and puts them in a Latex compilable .tex-file.//
+Python and Latex need to be installed on your system in order to perform the code generation and compilation afterwards.//
+Please be aware that you have to adapt the graphics path in ./templates/document_begin.tex and put a subject of testing to create the documents.
--- questionscrambler.py
+++ questionscrambler.py
@@ -49,32 +49,48 @@ def insert(ques):
             #  print(item)
             mytexfile.write(item)
 
+
 def mixer(liste):
     scrambled = []
     while len(liste) > 0:
         ind = randint(0, len(liste) - 1)
-       # print(ind)
+        # print(ind)
         scrambled.append(liste[ind])
         del liste[ind]
     return scrambled
 
-#def nexttest(points):
+
+# def nexttest(points):
 
 
 if __name__ == "__main__":
 
-   # count = input('Tell me how many Tests you would like to generate\n')
+    count = input('Tell me how many tests you would like to generate\n')
 
-
-    questionlist,totalpoints = readfile()
+    questionlist, totalpoints = readfile()
     print('Totalpoints = ' + str(totalpoints))
     list_to_scramble = questionlist.copy()
     scrambledlist = mixer(list_to_scramble)
     # print(questionlist)
 
+    with open(questiondir + 'nexttest.tex', 'w') as mytexfile:
+        mytexfile.write("\\vspace*{4.5cm}\n"
+                        "\\begin{flushright}\n"
+                        "\\scalebox{1.2}{\n"
+                        "\\begin{tabular}{|m{1.5cm}|m{1.5cm}|}\n"
+                        "\\hline\n"
+                        "total points &  your points \\\ \n"
+                        "\\hline\n" +
+                        str(totalpoints) + " & \\\ \n"
+                                           "\\hline\n"
+                                           "\\end{tabular}}\n"
+                                           "\\end{flushright}\n"
+                                           "\\clearpage\n"
+                                           "\\blankpage\n\n")
+
     copytemplate(templatedir + 'document_begin.tex', outputdir + 'automated.tex')
 
-    for x in range(0,1):
+    for x in range(0, int(count)):
         list_to_scramble = questionlist.copy()
         scrambledlist = mixer(list_to_scramble)
         insert("\\begin{enumerate}\n\\setcounter{page}{1}\n\n")
@@ -91,31 +107,16 @@ if __name__ == "__main__":
                        "\\end{flushright}\n" % (questionanwer.question, questionanwer.points)
 
             if questionanwer.pic is not None:
-                question = question +\
+                question = question + \
                            "\\begin{flushleft}\n" \
                            "\includegraphics[height=5cm]{europe.jpg}\n" \
                            "\end{flushleft}\n" \
-         #                  "\\vspace{5cm}\n\n"
+                    #                  "\\vspace{5cm}\n\n"
             insert(question)
         insert("\\end{enumerate}\n\n")
         # testseperator
 
-        with open(questiondir + 'nexttest.tex', 'w') as mytexfile:
-            mytexfile.write("\\vspace*{4.5cm}\n"
-                            "\\begin{flushright}\n"
-                            "\\scalebox{1.2}{\n"
-                            "\\begin{tabular}{|m{1.5cm}|m{1.5cm}|}\n"
-                            "\\hline\n"
-                            "total points &  your points \\\ \n"
-                            "\\hline\n" +
-                            str(totalpoints) + " & \\\ \n"
-                            "\\hline\n"
-                            "\\end{tabular}}\n"
-                            "\\end{flushright}\n"
-                            "\\clearpage\n"
-                            "\\blankpage\n\n")
-
         copytemplate(questiondir + 'nexttest.tex', outputdir + 'automated.tex')
-        #copytemplate(templatedir + 'next_test.tex', outputdir + 'automated.tex')
+        # copytemplate(templatedir + 'next_test.tex', outputdir + 'automated.tex')
 
     copytemplate(templatedir + 'document_end.tex', outputdir + 'automated.tex')
